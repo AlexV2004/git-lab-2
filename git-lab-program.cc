c77a24eaa9303001c316635fc30750b59cd3d65e
AlexV2004 <143838043+AlexV2004@users.noreply.github.com>
2024-01-26 15:00:41 -0500

    Create git-lab-program.cc
    Added program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Alex Vano
+ *        Date: 1/26/2024
+ * Description: Git Lab Program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
